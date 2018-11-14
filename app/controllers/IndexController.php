faec107497a47e919d5984d4452527d69d38c79b
lhz <l19517863@163.com>
2018-11-14 17:41:52 +0800

    Update app/controllers/IndexController.php

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -2,6 +2,8 @@
 
 namespace app\controllers;
 
+use Firebase\JWT\JWT;
+
 use core\Request;
 use app\models\User;
 
@@ -13,22 +15,52 @@ class IndexController extends Controller {
         view('index.index');
     }
     function chat(){
-        view('chat.chat');
+        view('chat.chat',['token_jwt',$_SESSION['token_jwt']]);
     }
 
     function login(){
-
+        $jwt_key = 'key123';
+        if(isset($_COOKIE['uuu'])){
+            redirect(Route('index.dologin'));
+        }
         return view('login.login');
     }
 
     function dologin(Request $req){
-        $data = $req->all();
-        // dd($data);
-        $user = (new User)->where(['uname'=>$req->uname,'password'=>$req->password])->first();
+        $jwt_key = 'key123';
+        // $data = $req->all();dd($data);
+        $remenber = $req->remember==null? false:true;
+
+        // 登陆页 检测cookie 直接跳转登陆古
+        if(isset($_COOKIE['uuu'])){
+            $data = JWT::decode($_COOKIE['uuu'], $jwt_key, array('HS256'));
+            $user = (new User)->where(['uname'=>$data->uname,'password'=>$data->password])->first();
+        }else{
+            $user = (new User)->where(['uname'=>$req->uname,'password'=>$req->password])->first();
+        }
+        
         // dd($user);
         if($user){
             $_SESSION['uid'] = $user['uid'];
             $_SESSION['uname'] = $user['uname'];
+
+            $cookie = array(
+                "uname" => $req->uname,
+                "password" => $req->password
+            );
+
+            $token = array(
+                "iat" => time(),   // 签发时间
+                "exp" => mktime(0, 0, 0, date('m'), date('d')+1, date('Y')),    // 过期时间 
+                "uid" => $user['uid'],
+                "uname" => $user['uname'],
+                "avatar" => "/img/head/2015.jpg"
+            );
+            $cookie_jwt = JWT::encode($cookie,$jwt_key); // 生成 JWT 的 cookie
+            $token_jwt = JWT::encode($token,$jwt_key); // 生成 token
+            $_SESSION['token_jwt'] = $token_jwt; // 用于 index.chat
+            response()->WithCookie('uuu',$cookie_jwt,strtotime("+1 day")); // 响应设置Cookie
+            dd($token);
             message('登陆成功',1,Route('index.chat'),2);
         }else{
             return message('用户不存在 或密码错误',1,Route('index.login'),2);
